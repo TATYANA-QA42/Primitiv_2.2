e56f97d20d92525d82f8262700136be026e2d108
Татьяна Пислегина <ztv-1988@yandex.ru>
2022-07-14 01:41:50 +0500

    file

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int balance = 0;
+        int increase = 800;
+        if (increase >= 1000) {
+            int bonus = increase / 100 * 1;
+            int summa = balance + increase + bonus;
+            //System.out.println("Баланс:" + balance);
+            //System.out.println("Пополнение:" + increase);
+            System.out.println("Бонус:" + bonus);
+            System.out.println("Сумма:" + summa);
+
+        }
+    else {
+            int bonus = 0;
+            int summa = balance + increase;
+            //System.out.println("Баланс:" + balance);
+            //System.out.println("Пополнение:" + increase);
+            System.out.println("Бонус:" + bonus);
+            System.out.println("Сумма:" + summa);
+
+        }
+    }
+}
